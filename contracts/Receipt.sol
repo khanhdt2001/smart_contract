9bd9331068efd7d08646e306a4c5ab26b158e5c3
khanhdt2001 <khanhrhino1607@gmail.com>
2023-01-17 21:03:23 +0700

    update code

--- contracts/Receipt.sol
+++ contracts/Receipt.sol
@@ -2,19 +2,17 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 interface Receipt {
     
     struct ReceiptDetail {
-        address originalOwner;
-        address currentOwner;
+        address vendor;
+        address lender;
         ERC721 NFTAddress;
         uint256 tokenId;
-        ERC20 ERC20Address;
-        uint256 ERC20Amount;
-        uint256 ERC20Rate;
+        uint256 tokenAmount;
+        uint256 tokenRate;
         uint256 amountOfTime;
         uint256 deadLine;
     }
