e5b3644be0d7ffefa0157f2db0e94fa7f12bbca0
khanhdt <khanhdt@smartosc.com>
2023-01-17 15:29:27 +0700

    make offer.sol, update logic code in LendingFactorty.sol

---
+++ contracts/Offer.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+interface Offer {
+    
+    struct OfferDetail {
+        uint256 offerERC20Amount;
+        uint256 offerAmountOfTime;
+    }
+    
+}
